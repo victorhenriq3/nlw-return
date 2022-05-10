b409739d79bf22b39ea7b43d981e753a610596fc
victorhenriqc <vic.1601@outlook.com>
2022-05-10 17:23:00 -0300

    add a bottom sheet

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -2,7 +2,7 @@ import 'react-native-gesture-handler'
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { View } from 'react-native';
-import { Widget } from './src/components/Widget';
+import Widget from './src/components/Widget';
 import AppLoading from 'expo-app-loading';
 import { useFonts, Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
 import { theme } from './src/theme';
--- mobile/src/components/Widget/index.tsx
+++ mobile/src/components/Widget/index.tsx
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { View, TouchableOpacity} from 'react-native';
 import { ChatTeardropDots } from "phosphor-react-native"
 import { styles } from './styles';
 import { theme } from '../../theme';
+import BottomSheet from '@gorhom/bottom-sheet';
+import { gestureHandlerRootHOC } from 'react-native-gesture-handler'
 
-export function Widget() {
+function Widget() {
+
+  const bottomSheetRef = useRef<BottomSheet>(null)
+
+  function handleOpen(){
+    bottomSheetRef.current?.expand()
+  }
+
   return (
     <>
-      <TouchableOpacity style={styles.button}>
+      <TouchableOpacity style={styles.button} onPress={handleOpen}>
         <ChatTeardropDots size={24} color={theme.colors.text_on_brand_color} weight="bold"/>
       </TouchableOpacity>
+
+      <BottomSheet 
+        ref={bottomSheetRef}
+        snapPoints={[1, 280]}  
+        backgroundStyle={styles.indicator}  
+      >
+
+      </BottomSheet>
     </>
   );
 }
+
+export default gestureHandlerRootHOC(Widget)
