28b13b6ef96acdf42e9a75c82b4d5545bcec8975
victorhenriqc <vic.1601@outlook.com>
2022-05-09 22:01:05 -0300

    finish test and add a cors

--- server/src/usecases/submitFeedbackUseCase.spec.ts
+++ server/src/usecases/submitFeedbackUseCase.spec.ts
@@ -1,18 +1,26 @@
 import { SubmitFeedbackUseCase } from "./submitFeedbackUseCase"
 
+const createFeedbackSpy = jest.fn();
+const sendMailsSpy = jest.fn()
+
 const submitFeedback = new SubmitFeedbackUseCase(
-    {create: async() => {}},
-    {sendMail: async() => {}}
+    {create: createFeedbackSpy},
+    {sendMail: sendMailsSpy}
 )
+
+//spies
 
 describe('Submit feedback', () => {
     it('should be able to submit a feddback', async () => {
 
-        expect(submitFeedback.execute({
+        await expect(submitFeedback.execute({
             type: 'bug',
             comment: 'exemple comment',
             screenshot: 'data:image/png;base64asdasd'
         })).resolves.not.toThrow()
+
+        expect(createFeedbackSpy).toHaveBeenCalled();
+        expect(sendMailsSpy).toHaveBeenCalled()
     })
     
     it('should not be able to submit without type', async () => {
@@ -21,6 +29,24 @@ describe('Submit feedback', () => {
             type: '',
             comment: 'exemple comment',
             screenshot: 'data:image/png;base64asdasd'
+        })).rejects.toThrow()
+    })
+    
+    it('should not be able to submit without comment', async () => {
+        
+        expect(submitFeedback.execute({
+            type: 'type',
+            comment: '',
+            screenshot: 'data:image/png;base64asdasd'
+        })).rejects.toThrow()
+    })
+    
+    it('should not be able to submit with an invalid screenshot', async () => {
+        
+        expect(submitFeedback.execute({
+            type: 'type',
+            comment: 'ta tudo bugado',
+            screenshot: 'teste.jpg'
         })).rejects.toThrow()
     })
 })
